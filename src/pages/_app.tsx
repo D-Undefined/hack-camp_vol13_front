b589cbbbea8efa8747d6332f5a0b44450fa4b3e5
kazuki-komori <komokazu.nankai@gmail.com>
2022-01-21 00:33:14 +0900

    add: base style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import type { AppProps } from "next/app"
-import "tailwindcss/tailwind.css"
 import { store } from "@/redux/store"
+import "@/styles/global.css"
+import type { AppProps } from "next/app"
 import { Provider } from "react-redux"
+import "tailwindcss/tailwind.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   mode: "jit",
   content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
-  // content: [],
   theme: {
+    fontFamily: {
+      sans: ["SFPro", "Arial", "Noto Sans JP", "Noto Sans KR", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [],

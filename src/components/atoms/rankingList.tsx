2190ceef4474549ed1ad5bae50c0b0b3db43d9ae
Kouta-fd <kotafd0124@icloud.com>
2022-01-23 01:45:46 +0900

    fix:ranking

--- src/components/atoms/rankingList.tsx
+++ src/components/atoms/rankingList.tsx
@@ -7,19 +7,12 @@ export const RankingList: FC = () => {
   return (
     <div className="grid grid-cols-12 items-center px-3 text-center text-gray-400 hover:bg-yellow-50 duration-300 cursor-pointer">
       <span>1</span>
-      <div>
-        <GiDiamondTrophy className="mx-auto text-4xl text-cyan-500"/>
-      </div>
-      <Avatar/>
-      <span className="col-span-4 text-gray-600">kazuki komori</span>
+      <Avatar className="col-span-2 text-center" />
+      <span className="col-span-3 text-gray-600 text-left">kazuki komori</span>
       <span className="col-span-2">9 か月前に参加</span>
       <div className="flex col-span-3">
-        <FaMedal className="mx-auto text-xl text-yellow-600"/>
-        <span className="font-bold text-yellow-600">10</span>
-        <FaMedal className="mx-auto text-xl text-zinc-400"/>
-        <span className="font-bold text-zinc-400">10</span>
-        <FaMedal className="mx-auto text-xl text-amber-700"/>
-        <span className="font-bold text-amber-700">10</span>
+        <GiDiamondTrophy className="mx-auto text-4xl text-yellow-500" />
+        <GiDiamondTrophy className="mx-auto text-4xl text-gray-400" />
       </div>
     </div>
   )
--- src/components/pages/rankings/SecRankings.tsx
+++ src/components/pages/rankings/SecRankings.tsx
@@ -6,11 +6,10 @@ export const SecRankings: FC = () => {
   return (
     <div className="my-12 min-h-screen">
       <AppContainer>
-        <div className="grid grid-cols-12 mb-3 text-center text-gray-500">
+        <div className="grid grid-cols-12 mb-3 text-center text-gray-500 px-3 border-b border-gray-300 pb-3">
           <span>Rank</span>
-          <span>Tier</span>
-          <span className="col-span-7 ml-2 text-left">User</span>
-          <span className="col-span-3">Medals</span>
+          <span className="col-span-7 ml-2 text-center">User</span>
+          <span className="col-span-3">Trophy</span>
         </div>
         <RankingList/>
         <RankingList/>
